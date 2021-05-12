cab44c34930df45b4a7bac36cff92adb2514d2e0
khiati rezkellah noureddine <46073873+noureddinekhiati@users.noreply.github.com>
2021-05-12 17:15:45 +0200

    add conv2D

--- BinaryNetwork.py
+++ BinaryNetwork.py
@@ -18,3 +18,23 @@ class BinaryLinear(nn.Linear):
 
 
 
+class BinaryConv2d(nn.Conv2d):
+
+    def forward(self, input):
+        bw = stand_bin(self.weight)
+        return F.conv2d(input, bw, self.bias, self.stride,
+                               self.padding, self.dilation, self.groups)
+
+    def reset_parameters(self):
+        # Glorot initialization
+        in_features = self.in_channels
+        out_features = self.out_channels
+        for k in self.kernel_size:
+            in_features *= k
+            out_features *= k
+        stdv = math.sqrt(1.5 / (in_features + out_features))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.zero_()
+
+        self.weight.lr_scale = 1. / stdv
